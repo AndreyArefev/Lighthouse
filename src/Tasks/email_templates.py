2ed6ce4e28976848f1f54ad3e428d7981c78b791
AndreyArefev <Sawyer_92_andrey@mail.ru>
2023-07-14 23:41:39 +0300

    celery 0.3

---
+++ src/Tasks/email_templates.py
@@ -0,0 +1,16 @@
+from email.message import EmailMessage
+from src.config import SMTP_USER
+from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
+
+def create_verified_email(email_to, email_from, confirm_token):
+    email = EmailMessage()
+    environment = Environment(loader=FileSystemLoader("src/Templates/"))
+    template = environment.get_template("email.html")
+    email["Subject"] = "Верификация пользователя"
+    email["From"] = SMTP_USER
+    email["To"] = email_to
+    email.set_content(template.render(token=confirm_token),
+                      subtype='html')
+    return email
+
